02e0277b46f575ff506b03ec120075d4221ebe15
PatrikMojzis06 <patrikmojzis26@gmail.com>
2024-06-08 23:02:41 +0200

    commit try

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="">
-      <h1>Welcome to my portfolio!!!</h1>
+      <h1>Welcome to mys portfolio!!!</h1>
     </main>
   );
 }
